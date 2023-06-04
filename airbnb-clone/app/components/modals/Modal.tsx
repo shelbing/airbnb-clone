64fc888f72b5d774aba1b9812bc932827e41c4ce
Dr. Steffen Helbing <steffen.helbing@compleneo.de>
2023-06-04 17:15:35 +0000

    Added modal

---
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useCallback, useEffect,useState } from "react";
+
+interface ModalProps{
+    isOpen: boolean;
+    onClose: ()=>void;
+    onSubmit: ()=>void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?:boolean;
+    secondaryAction?: ()=>void;
+    secondaryActionLabel?: string;
+}
+
+const Modal : React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryActionLabel
+    }) => {
+    
+    const [showModal,setShowModal] =useState(isOpen);
+
+    useEffect(()=>{
+        setShowModal(isOpen);
+    },[isOpen])
+        
+    const handleClose = useCallback(()=>{
+        if (disabled) return;
+        setShowModal(false);
+        setTimeout(()=>{
+            onClose();
+        },300)
+
+    },[disabled,onClose])
+
+        return (  
+            <div></div>
+    );
+}
+ 
+export default Modal;
--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 import Navbar from './components/navbar/navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
@@ -17,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal/>
+          <Navbar/>
+        </ClientOnly>
         {children}
         </body>
     </html>
